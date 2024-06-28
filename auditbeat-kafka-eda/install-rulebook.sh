8323ae7f9b48c1c22e62adb7a53a4482e2e7655f
root <root@ansible-rolebook.addc.local>
2024-06-28 15:10:13 +0700

    update code webhook

---
+++ auditbeat-kafka-eda/install-rulebook.sh
@@ -0,0 +1,14 @@
+
+sudo apt-get install python3-pip
+
+apt update && apt install -y build-essential maven  vim curl openjdk-17-jdk python3-dev python3-pip
+export JDK_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export JAVA_HOME=$JDK_HOME
+export PIP_NO_BINARY=jpy
+export PATH=$PATH:~/.local/bin
+python3 -m pip install -U Jinja2
+python3 -m pip install aiokafka
+python3 -m pip install ansible ansible-rulebook ansible-runner wheel
+ansible-galaxy collection install community.general ansible.eda
+
+pip3 install importlib-resources
